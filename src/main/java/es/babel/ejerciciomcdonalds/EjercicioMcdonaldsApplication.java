6e89cbb4dbe23d9cd93b808416390843af86ffb0
Mireya Medalle Merino <mireya.medalle@babelgroup.com>
2024-04-12 10:11:33 +0200

    Init

---
+++ src/main/java/es/babel/ejerciciomcdonalds/EjercicioMcdonaldsApplication.java
@@ -0,0 +1,13 @@
+package es.babel.ejerciciomcdonalds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioMcdonaldsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioMcdonaldsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/babel/ejerciciomcdonalds/EjercicioMcdonaldsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.babel.ejerciciomcdonalds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioMcdonaldsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
