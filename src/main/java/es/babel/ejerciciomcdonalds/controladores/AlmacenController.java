a1c34a317e1103c236a7f1300d14ab4cd239cc3e
Mireya Medalle Merino <mireya.medalle@babelgroup.com>
2024-05-07 08:36:02 +0200

    Documentacion API almacen y producto

--- src/main/java/es/babel/ejerciciomcdonalds/controladores/AlmacenController.java
+++ src/main/java/es/babel/ejerciciomcdonalds/controladores/AlmacenController.java
@@ -2,12 +2,31 @@ package es.babel.ejerciciomcdonalds.controladores;
 
 import es.babel.ejerciciomcdonalds.model.Producto;
 import es.babel.ejerciciomcdonalds.servicios.AlmacenService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/almacen")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Controlador del almacén",
+                version = "0.0",
+                description = "API McDonalds",
+                license = @License(name = "Apache 2.0", url = "http://foo.bar"),
+                contact = @Contact(name = "Mireya", email = "mireya.medalle@babelgroup.com")
+        )
+)
 public class AlmacenController {
     private final AlmacenService almacenService;
 
@@ -16,21 +35,40 @@ public class AlmacenController {
     }
 
     @GetMapping(value = "/init")
+    @Operation(summary = "Inicializar la base de datos",
+    description = "Se inicializará el almacén con los productos por defecto")
     public void inicializarAlmacen() {
         almacenService.inicializarAlmacen();
     }
 
     @GetMapping(value = "/list")
+    @Operation(summary = "Listar productos del almacen")
+    @ApiResponse(responseCode = "200", description = "Lista de productos",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Producto.class))
+                    )}
+    )
+    @ApiResponse(responseCode = "400", description = "Error 400")
     public List<Producto> listarProductos() {
         return almacenService.listarProductos();
     }
 
     @PostMapping(value = "/add")
-    public void push( @RequestBody Producto producto) {
+    @Operation(summary = "Añadir productos al almacen",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Producto añadido"),
+                    @ApiResponse(responseCode = "400", description = "No se ha podido añadir el producto")}
+    )
+    public void push(@Valid @RequestBody Producto producto) {
         almacenService.push(producto);
     }
 
     @DeleteMapping(value = "/delete/{id}")
+    @Operation(summary = "Borrar productos del almacen")
+    @ApiResponse(responseCode = "200", description = "Producto eliminado")
+    @ApiResponse(responseCode = "400", description = "No se pudo borrar el producto")
     public Producto pop( @PathVariable("id") int id ) {
         return almacenService.pop(id);
     }
--- src/main/java/es/babel/ejerciciomcdonalds/model/Producto.java
+++ src/main/java/es/babel/ejerciciomcdonalds/model/Producto.java
@@ -1,15 +1,27 @@
 package es.babel.ejerciciomcdonalds.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name="producto", description="Modelo de producto")
 public class Producto {
+    @Schema(description = "Identificador del producto", example = "1")
+    @Min(value = 1, message = "Debe ser mínimo 1")
     private int id;
+    @Schema(description = "Nombre del producto", example = "hamburguesa")
+    @NotBlank(message = "El producto tiene que tener nombre")
     private String nombre;
+    @Min(value = 0, message = "No puede haber cantidad negativa de producto")
+    @Schema(description = "Cantidad del producto disponible en el almacén", example = "20")
     private int cantidad;
 
 }
